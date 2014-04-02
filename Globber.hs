8ee0985dde74593cbdd01137d9fe9935f3f05236
Nikhil Desai <nikhild@stanford.edu>
2014-04-01 17:39:48 -0700

    new abstraction: GlobAtomList, containing the tokens to match against

--- Globber.hs
+++ Globber.hs
@@ -2,6 +2,14 @@ module Globber (matchGlob) where
 
 type GlobPattern = String
 
+type GlobAtomList = [GlobAtom]
+data GlobAtom = AnyCharacter | AnyString | AnyOf String | InRange (Char,Char)
+
 matchGlob :: GlobPattern -> String -> Bool
 matchGlob pattern str = pattern == str
 
+toAtomList :: GlobPattern -> GlobAtomList
+toAtomList = undefined
+
+matchAtomList :: GlobAtomList -> String -> Bool
+matchAtomList = undefined
