c9a95015a6e861b6a45f8f75cbcf83ea49d9d42d
Nikhil Desai <nikhild@stanford.edu>
2014-04-01 19:40:42 -0700

    rename AtomList to AtomSequence

--- Globber.hs
+++ Globber.hs
@@ -2,14 +2,14 @@ module Globber (matchGlob) where
 
 type GlobPattern = String
 
-type GlobAtomList = [GlobAtom]
-data GlobAtom = AnyCharacter | AnyString | AnyOf String | InRange (Char,Char)
+type AtomSequence = [Atom]
+data Atom = AnyCharacter | AnyString | AnyOf String | InRange (Char,Char)
 
 matchGlob :: GlobPattern -> String -> Bool
 matchGlob pattern str = pattern == str
 
-toAtomList :: GlobPattern -> GlobAtomList
-toAtomList = undefined
+toAtomSequence :: GlobPattern -> AtomSequence
+toAtomSequence = undefined
 
-matchAtomList :: GlobAtomList -> String -> Bool
-matchAtomList = undefined
+matchAtomSequence :: AtomSequence -> String -> Bool
+matchAtomSequence = undefined
