ef0316a12e2eb70f4777db5e917e1ce8116cf9c8
Nikhil Desai <nikhild@stanford.edu>
2014-04-05 20:55:22 -0700

    add support for ranges and escaping within ranges

--- Globber.hs
+++ Globber.hs
@@ -3,7 +3,7 @@ module Globber (matchGlob) where
 type GlobPattern = String
 
 type AtomSequence = [Atom]
-data Atom = Any | AnyString | AnyOf String | Literal Char
+data Atom = Any | AnyString | AnyOf String | Literal Char deriving (Show)
 
 matchGlob :: GlobPattern -> String -> Bool
 matchGlob = matchAtomSequence . toAtomSequence
@@ -14,10 +14,22 @@ toAtomSequence (x:xs) = case x of
                              '*' -> AnyString:toAtomSequence xs
                              '?' -> Any:toAtomSequence xs
                              '\\' -> nextLiteral xs
+                             '[' -> toRangeAtomSequence [] xs
+                             ']' -> error "Range closure without corresponding range opener"
                              _   -> (Literal x):toAtomSequence xs
                         where
                              nextLiteral (y:ys) = (Literal y):toAtomSequence ys
                              nextLiteral [] = error "Terminal escape character"
+
+toRangeAtomSequence :: String -> GlobPattern -> AtomSequence
+toRangeAtomSequence l [] = error "Range ended without closure!" 
+toRangeAtomSequence l s@(x:xs) = case x of
+                                    ']' -> (AnyOf l):toAtomSequence xs
+                                    '\\' -> nextLiteral xs
+                                    _   -> toRangeAtomSequence (x:l) xs
+                                 where
+                                    nextLiteral (y:ys) = toRangeAtomSequence (y:l) ys
+                                    nextLiteral [] = error "Range ended without closure!"
 
 matchAtomSequence :: AtomSequence -> String -> Bool
 matchAtomSequence a@(ah:as) s@(sh:ss) = case ah of
