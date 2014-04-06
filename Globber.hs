4e7be0b73d6cc65cdd535aaeaaf67a6ad33003cb
Nikhil Desai <nikhild@stanford.edu>
2014-04-05 20:08:49 -0700

    add framework for matching atom sequence; default toAtomSequence behavior to create literal string

--- Globber.hs
+++ Globber.hs
@@ -3,19 +3,23 @@ module Globber (matchGlob) where
 type GlobPattern = String
 
 type AtomSequence = [Atom]
-data Atom = Any | AnyString | AnyOf String | AnyIn (Char,Char)
+data Atom = Any | AnyString | AnyOf String | Literal Char
 
 matchGlob :: GlobPattern -> String -> Bool
 matchGlob = matchAtomSequence . toAtomSequence
 
 toAtomSequence :: GlobPattern -> AtomSequence
 toAtomSequence []   = []
-toAtomSequence (x:xs) = toAtomSequence xs
+toAtomSequence (x:xs) = (Literal x):toAtomSequence xs
 
 matchAtomSequence :: AtomSequence -> String -> Bool
-matchAtomSequence [] = null -- if our sequence is null, our string must be as well
-matchAtomSequence (a:as) = case a of
-                              Any         -> matchAtomSequence as
-                              AnyString   -> matchAtomSequence as
-                              AnyOf s     -> matchAtomSequence as
-                              AnyIn (c,d) -> matchAtomSequence as
+matchAtomSequence a@(ah:as) s@(sh:ss) = case ah of
+                              Any       -> matchAtomSequence as ss 
+                              AnyString -> matchAtomSequence as s || matchAtomSequence a ss
+                              AnyOf str -> (sh `elem` str) && matchAtomSequence as ss
+                              Literal c -> (sh == c) && matchAtomSequence as ss
+matchAtomSequence (ah:as) [] = case ah of
+                              AnyString -> matchAtomSequence as []
+                              _         -> False
+matchAtomSequence [] [] = True
+matchAtomSequence [] _ = False 
