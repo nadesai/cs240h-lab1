ac65aaab64b04b4fe139e795200fb1de2c306fa6
Nikhil Desai <nikhild@stanford.edu>
2014-04-07 10:09:30 -0700

    fix case indentation

--- Globber.hs
+++ Globber.hs
@@ -11,35 +11,35 @@ matchGlob = matchAtomSequence . toAtomSequence
 toAtomSequence :: GlobPattern -> AtomSequence
 toAtomSequence []   = []
 toAtomSequence (x:xs) = case x of
-                             '*' -> AnyString:toAtomSequence xs
-                             '?' -> AnyChar:toAtomSequence xs
-                             '\\' -> case xs of
-                                          [] -> error "Terminal escape character"
-                                          (y:ys) -> (Literal y):toAtomSequence ys
-                             '[' -> toRangeAtomSequence [] xs
-                             ']' -> error "Range closure without corresponding range opener"
-                             _   -> (Literal x):toAtomSequence xs
+                          '*'  -> AnyString:toAtomSequence xs
+                          '?'  -> AnyChar:toAtomSequence xs
+                          '\\' -> case xs of
+                                    [] -> error "Terminal escape character"
+                                    (y:ys) -> (Literal y):toAtomSequence ys
+                          '['  -> toRangeAtomSequence [] xs
+                          ']'  -> error "Range closure without corresponding range opener"
+                          _    -> (Literal x):toAtomSequence xs
 
 toRangeAtomSequence :: String -> GlobPattern -> AtomSequence
 toRangeAtomSequence _ [] = error "Range ended without closure" 
 toRangeAtomSequence l (x:xs) = case x of
-                                    ']' -> (AnyOf l):toAtomSequence xs
-                                    '\\' -> case xs of
-                                                 [] -> error "Range ended without closure"
-                                                 (y:ys) -> toRangeAtomSequence (y:l) ys
-                                    _   -> case xs of
-                                                 ('-':z:zs) | z /= ']' -> toRangeAtomSequence ([x..z] ++ l) zs --TODO: change to use Ord only
-                                                 ('-':'\\':z:zs) -> toRangeAtomSequence ([x..z] ++ l) zs
-                                                 _  -> toRangeAtomSequence (x:l) xs
+                                 ']'  -> (AnyOf l):toAtomSequence xs
+                                 '\\' -> case xs of
+                                           [] -> error "Range ended without closure"
+                                           (y:ys) -> toRangeAtomSequence (y:l) ys
+                                 _    -> case xs of
+                                           ('-':z:zs) | z /= ']' -> toRangeAtomSequence ([x..z] ++ l) zs --TODO: change to use Ord only
+                                           ('-':'\\':z:zs) -> toRangeAtomSequence ([x..z] ++ l) zs
+                                           _  -> toRangeAtomSequence (x:l) xs
 
 matchAtomSequence :: AtomSequence -> String -> Bool
 matchAtomSequence a@(ah:as) s@(sh:ss) = case ah of
-                              AnyChar   -> matchAtomSequence as ss 
-                              AnyString -> matchAtomSequence as s || matchAtomSequence a ss
-                              AnyOf str -> (sh `elem` str) && matchAtomSequence as ss
-                              Literal c -> (sh == c) && matchAtomSequence as ss
+                                          AnyChar   -> matchAtomSequence as ss 
+                                          AnyString -> matchAtomSequence as s || matchAtomSequence a ss
+                                          AnyOf str -> (sh `elem` str) && matchAtomSequence as ss
+                                          Literal c -> (sh == c) && matchAtomSequence as ss
 matchAtomSequence (ah:as) [] = case ah of
-                              AnyString -> matchAtomSequence as []
-                              _         -> False
+                                 AnyString -> matchAtomSequence as []
+                                 _         -> False
 matchAtomSequence [] [] = True
 matchAtomSequence [] _ = False 
