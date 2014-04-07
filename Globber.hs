ad232288d3de8d8b45eeea45d1bd5e9f75be0a61
Nikhil Desai <nikhild@stanford.edu>
2014-04-07 10:52:21 -0700

    first stab at using only Ord Char instances, rather than Enum

--- Globber.hs
+++ Globber.hs
@@ -3,7 +3,8 @@ module Globber (matchGlob) where
 type GlobPattern = String
 
 type AtomSequence = [Atom]
-data Atom = AnyChar | AnyString | AnyOf [Char] | Literal Char deriving (Show)
+data Atom = AnyChar | AnyString | AnyOf [RangeAtom] | Literal Char deriving (Show)
+data RangeAtom =  Lit Char | Range Char Char deriving (Show)
 
 matchGlob :: GlobPattern -> String -> Bool
 matchGlob = matchAtomSequence . toAtomSequence
@@ -20,24 +21,28 @@ toAtomSequence (x:xs) = case x of
                           ']'  -> error "Range closure without corresponding range opener"
                           _    -> (Literal x):toAtomSequence xs
 
-toRangeAtomSequence :: String -> GlobPattern -> AtomSequence
+toRangeAtomSequence :: [RangeAtom] -> GlobPattern -> AtomSequence
 toRangeAtomSequence _ [] = error "Range ended without closure" 
 toRangeAtomSequence l (x:xs) = case x of
                                  ']'  -> (AnyOf l):toAtomSequence xs
                                  '\\' -> case xs of
                                            [] -> error "Range ended without closure"
-                                           (y:ys) -> toRangeAtomSequence (y:l) ys
+                                           (y:ys) -> toRangeAtomSequence ((Lit y):l) ys
                                  _    -> case xs of
-                                           ('-':z:zs) | z /= ']' -> toRangeAtomSequence ([x..z] ++ l) zs --TODO: change to use Ord only
-                                           ('-':'\\':z:zs) -> toRangeAtomSequence ([x..z] ++ l) zs
-                                           _  -> toRangeAtomSequence (x:l) xs
+                                           ('-':z:zs) | z /= ']' -> toRangeAtomSequence ((Range x z):l) zs
+                                           ('-':'\\':z:zs) -> toRangeAtomSequence ((Range x z):l) zs
+                                           _  -> toRangeAtomSequence ((Lit x):l) xs
 
 matchAtomSequence :: AtomSequence -> String -> Bool
 matchAtomSequence a@(ah:as) s@(sh:ss) = case ah of
                                           AnyChar   -> matchAtomSequence as ss 
                                           AnyString -> matchAtomSequence as s || matchAtomSequence a ss
-                                          AnyOf str -> (sh `elem` str) && matchAtomSequence as ss
+                                          AnyOf list -> any (sh `inRange`) list && matchAtomSequence as ss
                                           Literal c -> (sh == c) && matchAtomSequence as ss
+                                        where
+                                          inRange :: Char -> RangeAtom -> Bool
+                                          inRange c (Lit d) = c == d
+                                          inRange c (Range a b) = c >= a && c <= b
 matchAtomSequence (ah:as) [] = case ah of
                                  AnyString -> matchAtomSequence as []
                                  _         -> False
